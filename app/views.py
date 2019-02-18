cfdf589db41f47c935a182ae005d41a970e26c49
Mon Maldonado <monterdi@gmail.com>
2019-02-18 18:01:41 +0100

    Plantilla formulario compra

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/nuevacompra')
+def nuevacompra():
+    return render_template('nuevacompra.html')
